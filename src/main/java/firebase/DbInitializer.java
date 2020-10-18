18c586f3befa8893a67e69dcb45b886bbd5f1bf7
dsiemens-coder <61662301+dsiemens-coder@users.noreply.github.com>
2020-10-18 17:59:40 +0200

    fix: file separator

--- src/main/java/firebase/DbInitializer.java
+++ src/main/java/firebase/DbInitializer.java
@@ -14,7 +14,7 @@ public class DbInitializer {
     try {
       //get current working dir
       String workingDir = System.getProperty("user.dir");
-      FileInputStream serviceAccount = new FileInputStream(workingDir+"/integrateit-41c60-firebase-adminsdk-ndl1c-136a553850.json");
+      FileInputStream serviceAccount = new FileInputStream(workingDir+System.getProperty("file.separator")+"integrateit-41c60-firebase-adminsdk-ndl1c-136a553850.json");
 
       FirebaseOptions options = new FirebaseOptions.Builder()
           .setCredentials(GoogleCredentials.fromStream(serviceAccount))
