6dfa7fbcb066e282e0d8d129f461872dfba16199
dsiemens-coder <61662301+dsiemens-coder@users.noreply.github.com>
2020-10-26 10:21:13 +0100

    feat: override equals method

--- src/main/java/openApi/OpenApiEntity.java
+++ src/main/java/openApi/OpenApiEntity.java
@@ -23,4 +23,16 @@ public class OpenApiEntity {
   public void setName(String name) {
     this.name = name;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    OpenApiEntity that = (OpenApiEntity) o;
+    return id.equals(that.id);
+  }
 }
